f7de0c2184b56434463708b1520477224ba7fe26
Priyanka Pandey <110595977+603977@users.noreply.github.com>
2023-04-17 20:02:01 +0530

    Create Welcome.java

---
+++ Welcome.java
@@ -0,0 +1,14 @@
+public class Welcome {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num=1;
+		while(num<5) {
+			System.out.println("while loop");
+			num++;
+		}
+		for(num=0;num<5;num++) {
+			System.out.println("while loop");
+		}
+	}
+  }
